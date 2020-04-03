ae94e6d53362fb9f4bed38cc1c3798f9401e8d62
ChetanPatil28 <pchetan481@gmail.com>
2020-04-03 20:24:19 +0530

    Added Header

--- projection.cpp
+++ projection.cpp
@@ -1,209 +1,174 @@
+#include<vector>
+#include "Utilities.h"
 #include "olcConsoleGameEngine.h"
-#include <iostream>
 
 using namespace std;
-#define PI 3.14159265
 
 
-struct vec3d
-{
-	float x, y, z;
-	vec3d operator+(vec3d p) { return { x + p.x, y + p.y, z+p.z }; }
-	vec3d operator-(vec3d p) { return { x - p.x, y - p.y, z - p.z }; }
-	vec3d operator/(float p) { return { x/p, y/p, z/p }; }
-	vec3d operator*(float p) { return { x * p, y * p, z * p }; }
-
+struct Vertex3i {
+	int x, y, z;
 };
 
-struct triangle
+ostream& operator<<(ostream& disp, Vertex3i p)
 {
-	vec3d p1;
-	vec3d p2;
-	vec3d p3;
-};
+	return disp << " (" << p.x << "," << p.y << "," << p.z << ")" << endl;
+}
 
-struct cube
+Vertex3i float_to_int(Vertex3D& p)
 {
-	vec3d p1;
-	vec3d p2;
-	vec3d p3;
-	vec3d p4;
-	vec3d p5;
-	vec3d p6;
-	vec3d p7;
-	vec3d p8;
-
-
-};
-
-struct square
-{
-	vec3d p1, p2, p3, p4;
-};
-
-
-ostream& operator<<(ostream& print, vec3d p)
-{
-	return print << "(" << p.x << "," << p.y << ")";
+	return { (int)p.x, (int)p.y, (int)p.z };
 }
 
 
-class Geometry : public olcConsoleGameEngine
+Vertex3D MatMul(float m[][4], Vertex3D p)
 {
+	Vertex3D temp;
+	//cout << "Vertex b4 multiply is " << p << endl;
 
-private:
-	triangle tri;
-	cube cub;
-	square sqr;
+	temp.x = (m[0][0] * p[0]) + m[0][3];
+	temp.y = (m[1][1] * p[1]) + m[1][3];
+	temp.z = (m[2][2] * p[2]) + m[2][3];
 
-	float Theta = 0;
-	float off = 5;
-	float cX = 50.0f  ;  
-	//float yy = m_nSc
-	float cY = 50.0f  ;
-	float cZ = 10.0f;
-	vec3d test_point = { cX + 35.0f , cY + 35.0f , 0.0f };
-	vec3d C = { cX, cY, cZ };
+	//cout << "X map to " << (int)temp.x << endl;
+	//cout << "Y map to " << (int)temp.y << endl;
+	//cout << "Z map to " << (int)temp.z << endl;
 
-	// YAW
-	void OrthoGraphicRotateZ(vec3d &i, vec3d &o,float theta)
+	//cout << "Vertex aftr multiply is " << temp << endl;
+
+
+	float w = m[3][3];
+	//float w = 1;
+	if (w != 0.0f)
 	{
-		vec3d diff = i - C;
-		o.x = (cosf(theta)*diff.x) - (sinf(theta)*diff.y) + C.x ;
-		o.y = (sinf(theta)*diff.x) + (cosf(theta)*diff.y) + C.y;
-		o.z = i.z;
-		o = o + C;
+		temp /= w;
 	}
+	return temp;
 
-	// PITCH
-	void OrthoGraphicRotateY(vec3d &i, vec3d &o, float theta)
+}
+
+void print_mat(float m[][4])
+{
+	//cout << "Matrix starts now" << endl;
+	for (int i = 0; i < 4; i++)
 	{
-		vec3d diff = i - C;
-		o.x = (cosf(theta)*diff.x) - (sinf(theta)*diff.z) + C.x;
-		o.y = i.y;
-		o.z = (sinf(theta)*diff.x) + (cosf(theta)*diff.z) + C.y;
-		o = o + C;
+		for (int j = 0; j < 4; j++)
+		{
+			//cout << m[i][j] << "\t";
+		}
+		//cout << "\n";
 	}
-
-	void ProjectiveRotateY(vec3d &i, vec3d &o, float theta)
-	{
-		//i = i / i.z;
-		vec3d diff = i - C;
-		o.x = (cosf(theta)*diff.x) - (sinf(theta)*diff.z) + C.x;
-		o.y = i.y;
-		o.z = (sinf(theta)*diff.x) + (cosf(theta)*diff.z) + C.y;
-		o = o + C;
-
-		//o = o * i.z;
-		o = o / o.z;
-
-	}
-
-	// ROLL
-	void OrthoGraphicRotateX(vec3d &i, vec3d &o, float theta)
-	{
-		vec3d diff = i - C;
-		o.x = i.x;
-		o.y = (cosf(theta)*diff.y) - (sinf(theta)*diff.z) + C.y ;
-		o.z = (sinf(theta)*diff.y) + (cosf(theta)*diff.z) + C.z;
-		o = o + C;
-	}
+}
 
 
+class olcEngine3D : public olcConsoleGameEngine
+{
 public:
-	Geometry()
+
+	string file;
+
+	olcEngine3D(string file_name)
 	{
-		m_sAppName = L"Projection";
+		m_sAppName = L"3D Demo";
+		file = file_name;
 	}
+
+	float mat[4][4];
+	vector<Triangle> tris;
+	float fTheta = 0;
+
+	float width = 200;
+	float height = 200;
+	Vertex3D C = { 0.0f, 0.0f, 0.0f };
+
+	Vertex3D OrthoGraphicRotateZ(Vertex3D &i, float &theta)
+	{
+		float alpha = cosf(theta);
+		float beta = sinf(theta);
+		Vertex3D o = { 0.0f, 0.0f, 0.0f };
+		o.x = (alpha * i.x) - (beta * i.y) + (C.x * (1 - alpha)) + (beta * C.y);
+		o.y = (beta * i.x) + (alpha * i.y) - (beta* C.x) + (C.y * (1 - alpha)) ;
+		o.z = i.z ;
+		return o;
+	}
+			
+	//                   PITCH
+	Vertex3D OrthoGraphicRotateY(Vertex3D &i, float &theta)
+	{
+		float alpha = cosf(theta);
+		float beta = sinf(theta);
+		Vertex3D o = { 0.0f, 0.0f, 0.0f };
+		o.x = (alpha * i.x) - (beta * i.z) + (C.x * (1 - alpha)) + (beta * C.z);
+		o.y = i.y ;
+		o.z = (beta * i.x) + (alpha * i.z) - (beta* C.z) + (C.z * (1 - alpha)) ;
+		return o;
+	}
+		
+	//                          ROLL
+	Vertex3D OrthoGraphicRotateX(Vertex3D &i, float &theta)
+	{
+		float alpha = cosf(theta);
+		float beta = sinf(theta);
+		Vertex3D o = { 0.0f, 0.0f, 0.0f };
+		o.x = i.x;
+		o.y = (alpha * i.y) - (beta * i.z) + (C.y * (1 - alpha)) + (beta * C.z);
+		o.z = (beta * i.y) + (alpha * i.z) - (beta* C.z) + (C.z * (1 - alpha)) ;
+		return o;
+	}
+	
+public:
 	bool OnUserCreate() override
 	{
-		tri.p1 = { 50.0f, 50.0f, 10.0f };
-		tri.p2 = {70.0f, 35.0f, 12.0f };
-		tri.p3 = { 90.0f, 75.0f, 20.0f };
-		
-		sqr.p1 = { 50.0f, 50.0f, 0.0f };
-		sqr.p2 = { 100.0f, 50.0f, 0.0f };
-		sqr.p3 = { 100.0f, 100.0f, 0.0f };
-		sqr.p4 = { 50.0f, 100.0f, 0.0f };
-
-		cub.p1 = { C.x-25.0f,   C.y-25.0f,   C.z - 25.0f };
-		cub.p2 = { C.x +25.0f,  C.y -25.0f,  C.z - 25.0f };
-		cub.p3 = { C.x +25.0f,  C.y +25.0f,  C.z - 25.0f };
-		cub.p4 = { C.x -25.0f,  C.y + 25.0f, C.z - 25.0f };
-		cub.p5 = { C.x - 25.0f, C.y - 25.0f, C.z + 25.0f };
-		cub.p6 = { C.x + 25.0f, C.y - 25.0f, C.z + 25.0f };
-		cub.p7 = { C.x + 25.0f, C.y + 25.0f, C.z + 25.0f };
-		cub.p8 = { C.x - 25.0f, C.y + 25.0f, C.z + 25.0f };
-
+		Mesh object(file);
+		object.buildMesh();
+		tris = object.triangles;
+		OrthoGraphicMatrix ogm(+8.0f, -8.0f, -8.0f, +8.0f, -8.0f, +8.0f);
+		ogm.create_matrix(mat);
 		return true;
 	}
-
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		Theta += 1.0f * fElapsedTime;
-
-		// Clear Screen
 		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
-		
-		cube op;
-		vec3d test_point_op;
+		fTheta += 1.0f * fElapsedTime;
+
+		for (int i = 0; i < tris.size(); i++)
+		{
+
+			Vertex3D v1 = OrthoGraphicRotateZ(tris[i].Point[0], fTheta);
+			Vertex3D v2 = OrthoGraphicRotateZ(tris[i].Point[1], fTheta);
+			Vertex3D v3 = OrthoGraphicRotateZ(tris[i].Point[2], fTheta);
+
+			 v1 = OrthoGraphicRotateY(v1, fTheta);
+			 v2 = OrthoGraphicRotateY(v2, fTheta);
+			 v3 = OrthoGraphicRotateY(v3, fTheta);
+
+			//Vertex3D v1 = MatMul(mat, tris[i].Point[0]);
+			//Vertex3D v2 = MatMul(mat, tris[i].Point[1]);
+			//Vertex3D v3 = MatMul(mat, tris[i].Point[2]);
 
 
-		OrthoGraphicRotateY(cub.p1, op.p1, Theta);
-		OrthoGraphicRotateY(cub.p2, op.p2, Theta);
-		OrthoGraphicRotateY(cub.p3, op.p3, Theta);
-		OrthoGraphicRotateY(cub.p4, op.p4, Theta);
-		OrthoGraphicRotateY(cub.p5, op.p5, Theta);
-		OrthoGraphicRotateY(cub.p6, op.p6, Theta);
-		OrthoGraphicRotateY(cub.p7, op.p7, Theta);
-		OrthoGraphicRotateY(cub.p8, op.p8, Theta);
+			v1 = MatMul(mat, v1);
+			v2 = MatMul(mat, v2);
+			v3 = MatMul(mat, v3);
 
-		//ProjectiveRotateY(cub.p1, op.p1, Theta);
-		//ProjectiveRotateY(cub.p2, op.p2, Theta);
-		//ProjectiveRotateY(cub.p3, op.p3, Theta);
-		//ProjectiveRotateY(cub.p4, op.p4, Theta);
-		//ProjectiveRotateY(cub.p5, op.p5, Theta);
-		//ProjectiveRotateY(cub.p6, op.p6, Theta);
-		//ProjectiveRotateY(cub.p7, op.p7, Theta);
-		//ProjectiveRotateY(cub.p8, op.p8, Theta);
+			//cout << "Tested point is " << test << endl;
 
-		OrthoGraphicRotateZ(test_point, test_point_op, Theta);
+			Vertex3D unit_vec{ 1.0f,1.0f,1.0f };
+			//cout << "Maping to screen " << ((test + unit_vec) / 2.0f) * 400 << endl;
+			Vertex3D raster_points1 = v1.Ndc_to_Raster(width, height);
+			Vertex3D raster_points2 = v2.Ndc_to_Raster(width, height);
+			Vertex3D raster_points3 = v3.Ndc_to_Raster(width, height);
 
-		DrawLine(op.p1.x,op.p1.y,op.p2.x,op.p2.y, PIXEL_SOLID, FG_WHITE);
-		DrawLine(op.p2.x, op.p2.y, op.p3.x, op.p3.y, PIXEL_SOLID, FG_WHITE);
-		DrawLine(op.p3.x, op.p3.y, op.p4.x, op.p4.y, PIXEL_SOLID, FG_WHITE);
-		DrawLine(op.p4.x, op.p4.y, op.p1.x, op.p1.y, PIXEL_SOLID, FG_WHITE);
-
-		DrawLine(op.p5.x, op.p5.y, op.p6.x, op.p6.y, PIXEL_SOLID, FG_WHITE);
-		DrawLine(op.p6.x, op.p6.y, op.p7.x, op.p7.y, PIXEL_SOLID, FG_WHITE);
-		DrawLine(op.p7.x, op.p7.y, op.p8.x, op.p8.y, PIXEL_SOLID, FG_WHITE);
-		DrawLine(op.p8.x, op.p8.y, op.p5.x, op.p5.y, PIXEL_SOLID, FG_WHITE);
-
-		DrawLine(op.p1.x, op.p1.y, op.p5.x, op.p5.y, PIXEL_SOLID, FG_WHITE);
-		DrawLine(op.p2.x, op.p2.y, op.p6.x, op.p6.y, PIXEL_SOLID, FG_WHITE);
-		DrawLine(op.p3.x, op.p3.y, op.p7.x, op.p7.y, PIXEL_SOLID, FG_WHITE);
-		DrawLine(op.p4.x, op.p4.y, op.p8.x, op.p8.y, PIXEL_SOLID, FG_WHITE);
+			DrawTriangle(raster_points1.x, raster_points1.y,
+				raster_points2.x, raster_points2.y,
+				raster_points3.x, raster_points3.y, PIXEL_SOLID, FG_WHITE);
 
 
 
+		}
 
-		DrawLine(test_point_op.x, test_point_op.y, test_point.x, test_point.y, PIXEL_SOLID, FG_WHITE);
-
-
-		//DrawTriangle(op.p1.x , op.p1.y,
-	//		op.p2.x, op.p2.y,
-	//		op.p3.x, op.p3.y,
-	//		PIXEL_SOLID, FG_WHITE);
-		//DrawCircle(test_point.x, test_point.y, 2, PIXEL_SOLID, FG_WHITE);
-
-
-		DrawCircle(test_point_op.x, test_point_op.y, 2, PIXEL_SOLID, FG_DARK_GREY);
-		 
-		return true;
-
+		return true; 
 	}
-
 };
 
 
@@ -211,10 +176,12 @@ public:
 
 int main()
 {
-	Geometry demo;
-	
+	string object_file = "TeaPot.obj";
+	olcEngine3D demo(object_file);
 	if (demo.ConstructConsole(200, 200, 2, 2))
 		demo.Start();
 	return 0;
+
+	return  0;
 }
 
